3f27245323c07a5a0e312151ed943690d2abe2e1
alu0100764666 <alu0100764666@ull.edu.es>
2014-12-18 21:48:46 +0000

    casi

--- lib/frutal/frutaltree.rb
+++ lib/frutal/frutaltree.rb
@@ -6,7 +6,7 @@ class FrutalTree
       @fruits=0
       
       @life= lambda do |x,y|
-         if(@age>50)
+         if(@age>20)
             @fruits=-1
          elsif(@age>7)
             @fruits+=(@age/3)+1
@@ -32,12 +32,40 @@ class FrutalTree
    
    def recolectar_una
       if (@fruits<0)
-         return "Lo sentimos mucho...su arbol ha muerto"
+         return "Lo sentimos mucho...su arbol ha muerto\n"
       elsif(@fruits>0)
          @fruits-=1
-         return "Estaba moderadamente deliciosa"
+         return "Estaba moderadamente deliciosa\n"
       else
-         return "No quedan frutas en el arbol"
+         return "No quedan frutas en el arbol\n"
       end
    end
 end
+
+arbol = FrutalTree.new
+
+th1 = Thread.new do
+    while arbol.age != -1 do
+        arbol.get_old
+        print "El arbolito se hizo un poco mas viejo\n"
+        sleep 1
+        print "Esperando 1 segundo para crecer\n"
+    end
+end
+
+th2 = Thread.new do 
+    while arbol.age != -1 do
+        if arbol.fruits == 0 then
+            print "No quedan frutas, a esperar! :D\n"
+            sleep 2
+       end
+       if arbol.age > 0 then
+        print arbol.recolectar_una
+      end
+    end
+    print "El arbolito murio D:\n"
+end
+
+
+th2.join
+th1.join
